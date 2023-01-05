0dd23ced1c694fc690b82089a4d64a43a1108776
LucasLon22 <crlucaslondero@gmail.com>
2023-01-04 22:33:20 -0300

    todo list sin estilo

---
+++ src/components/TodoForm.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+function TodoForm(props) {
+    const [tarea, setTarea] = useState(props.edit ? props.edit.value : '');
+
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        inputRef.current.focus();
+    });
+
+    const handleChange = e => {
+        setTarea(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 10000),
+            text: tarea
+        });
+        setTarea('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className='todo-form'>
+            {props.edit ? (
+                <>
+                    <input
+                        placeholder='Update your item'
+                        value={tarea}
+                        onChange={handleChange}
+                        name='text'
+                        ref={inputRef}
+                        className='todo-input edit'
+                    />
+                    <button onClick={handleSubmit} className='todo-button edit'>
+                        Update
+                    </button>
+                </>
+            ) : (
+                <>
+                    <input
+                        placeholder='Add a todo'
+                        value={tarea}
+                        onChange={handleChange}
+                        name='text'
+                        className='todo-input'
+                        ref={inputRef}
+                    />
+                    <button onClick={handleSubmit} className='todo-button'>
+                        Add todo
+                    </button>
+                </>
+            )}
+        </form>
+    );
+}
+
+export default TodoForm;
